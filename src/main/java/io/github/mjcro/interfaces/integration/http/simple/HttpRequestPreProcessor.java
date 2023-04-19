faaab8b79d12fa3f190858b643a6019af123dd42
Gotterdemarung <gotterdemarung@gmail.com>
2023-04-19 09:16:00 +0300

    :sparkles: HTTP request pre processor

---
+++ src/main/java/io/github/mjcro/interfaces/integration/http/simple/HttpRequestPreProcessor.java
@@ -0,0 +1,56 @@
+package io.github.mjcro.interfaces.integration.http.simple;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+@FunctionalInterface
+public interface HttpRequestPreProcessor {
+    /**
+     * Constructs HTTP response pre-processor from given function.
+     *
+     * @param function Function.
+     * @return HTTP request pre-processor.
+     */
+    static HttpRequestPreProcessor ofFunction(Function<HttpRequest, HttpRequest> function) {
+        Objects.requireNonNull(function, "function");
+        return function::apply;
+    }
+
+    /**
+     * Constructs HTTP request pre-processor from given unary operator.
+     *
+     * @param unaryOperator Unary operator.
+     * @return HTTP request pre-processor.
+     */
+    static HttpRequestPreProcessor ofUnary(UnaryOperator<HttpRequest> unaryOperator) {
+        Objects.requireNonNull(unaryOperator, "unaryOperator");
+        return unaryOperator::apply;
+    }
+
+    /**
+     * Constructs HTTP request pre-processor over given consumer.
+     * Constructed pre-processor will pass HTTP request to consumer and then
+     * return it unmodified.
+     *
+     * @param consumer Consumer to pass HTTP request into.
+     * @return HTTP request pre-processor.
+     */
+    static HttpRequestPreProcessor ofConsumer(Consumer<HttpRequest> consumer) {
+        Objects.requireNonNull(consumer, "consumer");
+        return request -> {
+            consumer.accept(request);
+            return request;
+        };
+    }
+
+    /**
+     * Performs pre-processing tasks, injecting or modifying
+     * data inside HTTP request.
+     *
+     * @param request HTTP request with original data.
+     * @return Altered HTTP request.
+     */
+    HttpRequest preProcess(HttpRequest request);
+}
