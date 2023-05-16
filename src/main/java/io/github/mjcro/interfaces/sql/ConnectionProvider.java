97350b2dd5fd6596b3559cf81f4122c382fbefa9
Gotterdemarung <gotterdemarung@gmail.com>
2023-05-16 09:47:50 +0300

    :sparkles: Invoke with connection

--- src/main/java/io/github/mjcro/interfaces/sql/ConnectionProvider.java
+++ src/main/java/io/github/mjcro/interfaces/sql/ConnectionProvider.java
@@ -3,6 +3,8 @@ package io.github.mjcro.interfaces.sql;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @FunctionalInterface
@@ -36,4 +38,31 @@ public interface ConnectionProvider {
      * @throws SQLException On connection error.
      */
     Connection getConnection() throws SQLException;
+
+    /**
+     * Runs given function passing database connection to it.
+     * After invocation connection will be closed by default implementation.
+     *
+     * @param function Function to invoke.
+     * @param <T>      Response type.
+     * @return Function response.
+     */
+    default <T> T invokeWithConnection(Function<Connection, T> function) throws SQLException {
+        try (Connection connection = getConnection()) {
+            return function.apply(connection);
+        }
+    }
+
+    /**
+     * Runs given consumer passing database connection to it.
+     * After invocation connection will be closed by default implementation.
+     *
+     * @param consumer Consumer to invoke.
+     */
+    default void invokeWithConnection(Consumer<Connection> consumer) throws SQLException {
+        this.invokeWithConnection(connection -> {
+            consumer.accept(connection);
+            return null;
+        });
+    }
 }
