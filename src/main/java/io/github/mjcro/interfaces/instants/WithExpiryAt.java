a3de9c90afaf2565488c75f48e0267d6f575a5a9
Gotterdemarung <gotterdemarung@gmail.com>
2025-01-27 19:00:59 +0200

    :sparkles: Expiration checks

--- src/main/java/io/github/mjcro/interfaces/instants/WithExpiryAt.java
+++ src/main/java/io/github/mjcro/interfaces/instants/WithExpiryAt.java
@@ -1,5 +1,6 @@
 package io.github.mjcro.interfaces.instants;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.Temporal;
@@ -36,5 +37,43 @@ public interface WithExpiryAt<T extends Temporal> {
      */
     default String formatExpiryAtISOInstant() {
         return DateTimeFormatter.ISO_INSTANT.format(getExpiryAtInstant());
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     *
+     * @param against Time to check expiration against (for most cases - current time).
+     * @return True if expired, false otherwise.
+     */
+    default boolean isExpired(Temporal against) {
+        return Duration.between(against, getExpiryAt()).isNegative();
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     *
+     * @param against Time to check expiration against (for most cases - current time).
+     * @return True if not expired, false otherwise.
+     */
+    default boolean isNotExpired(Temporal against) {
+        return !isExpired(against);
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     *
+     * @return True if expired, false otherwise.
+     */
+    default boolean isExpired() {
+        return isExpired(Instant.now());
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     *
+     * @return True if not expired, false otherwise.
+     */
+    default boolean isNotExpired() {
+        return isNotExpired(Instant.now());
     }
 }
--- src/main/java/io/github/mjcro/interfaces/instants/WithOptionalExpiryAt.java
+++ src/main/java/io/github/mjcro/interfaces/instants/WithOptionalExpiryAt.java
@@ -1,5 +1,6 @@
 package io.github.mjcro.interfaces.instants;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.Temporal;
@@ -54,5 +55,49 @@ public interface WithOptionalExpiryAt<T extends Temporal> {
      */
     default Optional<String> formatExpiryAtISOInstant() {
         return getExpiryAtInstant().map(DateTimeFormatter.ISO_INSTANT::format);
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     * Entity is not expired if it has no expiration data.
+     *
+     * @param against Time to check expiration against (for most cases - current time).
+     * @return True if expired, false otherwise.
+     */
+    default boolean isExpired(Temporal against) {
+        return getExpiryAt()
+                .map($ -> Duration.between(against, $).isNegative())
+                .orElse(false);
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     * Entity is not expired if it has no expiration data.
+     *
+     * @param against Time to check expiration against (for most cases - current time).
+     * @return True if not expired, false otherwise.
+     */
+    default boolean isNotExpired(Temporal against) {
+        return !isExpired(against);
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     * Entity is not expired if it has no expiration data.
+     *
+     * @return True if expired, false otherwise.
+     */
+    default boolean isExpired() {
+        return isExpired(Instant.now());
+    }
+
+    /**
+     * Checks if entity is expired according to its expiration time.
+     * Entity is not expired if it has no expiration data.
+     *
+     * @return True if not expired, false otherwise.
+     */
+    default boolean isNotExpired() {
+        return isNotExpired(Instant.now());
     }
 }
